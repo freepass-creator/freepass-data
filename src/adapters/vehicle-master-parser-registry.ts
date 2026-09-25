5e104a658a9556192d1496c23261e50edb73b167
freepass-creator <dudguq@gmail.com>
2026-09-25 22:49:19 +0900

    feat(vehicle-master): register historical Carisyou source parser

--- src/adapters/vehicle-master-parser-registry.ts
+++ src/adapters/vehicle-master-parser-registry.ts
@@ -1,10 +1,12 @@
 import { KiaOfficialPriceParser } from './kia-official-price-parser.js';
 import { CarnoonVehicleParser } from './carnoon-vehicle-parser.js';
+import { CarisyouHistoricalParser } from './carisyou-historical-parser.js';
 import type { VehicleMasterSourceParser } from '../ports/vehicle-master-source-parser.js';
 
 export function createVehicleMasterSourceParsers(): VehicleMasterSourceParser[] {
   return [
     new KiaOfficialPriceParser(),
     new CarnoonVehicleParser(),
+    new CarisyouHistoricalParser(),
   ];
 }
